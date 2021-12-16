bf09745c7c778f3e21088960f58c85dc28476a61
Potato <80370292+Potat05@users.noreply.github.com>
2021-12-15 19:41:36 -0600

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,24 @@
+
+
+const settings = {
+    // Background blur radius in pixels
+    background_blur: 3,
+
+    // Fade transition time from 1 video to the next
+    transition_time_ms: 2000,
+    // Time before the end of the video to queue next video to play
+    // Keep this above transition time
+    time_before_next: 5000,
+
+    backgrounds: [
+        "./resources/1.mkv",
+        "./resources/2.mkv",
+        "./resources/3.mkv",
+        "./resources/4.mkv",
+        "./resources/5.mkv",
+        "./resources/6.mkv",
+        "./resources/7.mkv",
+        "./resources/8.mkv",
+        "./resources/9.mkv"
+    ],
+}
---
+++ src/background.js
@@ -0,0 +1,74 @@
+
+
+// Give transitions & blur to the top and bottom elements
+document.querySelectorAll('.background > video').forEach(video => {
+    video.style.transition = `opacity ${settings.transition_time_ms}ms ease-in-out`;
+});
+
+document.querySelector('.background').style.filter = `blur(${settings.background_blur}px)`;
+
+
+// Start video on click
+document.addEventListener('click', () => {
+    document.querySelector('.top').play();
+});
+
+
+
+let current_background = -1;
+
+function setRandomBackground() {
+
+    // Get a random background
+    do {
+        var new_background = Math.floor(Math.random() * settings.backgrounds.length);
+    } while(new_background == current_background);
+    current_background = new_background;
+
+
+    // Get top & bottom elements
+    const top = document.querySelector('.top');
+    const bottom = document.querySelector('.bottom');
+
+    // Set new bg
+    const bg = settings.backgrounds[current_background];
+    bottom.setAttribute('src', bg);
+    bottom.setAttribute('type', `video/${bg.split('.').pop()}`);
+
+    // Wait till image is loaded to start transition
+    bottom.onloadeddata = () => {
+        if(bottom.readyState < 3) return;
+        top.classList.remove('top');
+        top.classList.add('bottom');
+        bottom.classList.remove('bottom');
+        bottom.classList.add('top');
+
+        bottom.play();
+        // Remove old bg to save resources (I think this helps)
+        setTimeout(() => {
+            top.pause();
+            top.removeAttribute('src');
+            changing = false;
+        }, settings.transition_time_ms);
+    }
+
+}
+
+
+const checkEndInterval = 100;
+let changing = false;
+
+setInterval(() => {
+    if(changing) return;
+
+    const top = document.querySelector('.top');
+
+    if(top.currentTime >= top.duration - settings.time_before_next*0.001) {
+        changing = true;
+        setRandomBackground();
+    }
+
+}, checkEndInterval)
+
+
+setRandomBackground();
